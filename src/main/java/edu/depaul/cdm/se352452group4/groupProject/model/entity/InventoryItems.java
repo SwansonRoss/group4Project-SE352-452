0f7ed74d617e4ec434e45624f944a12b1fa33444
Heather Duke <hduke@mail.depaul.edu>
2019-05-06 21:11:58 -0500

    created entity for inventoryItems

--- src/main/java/edu/depaul/cdm/se352452group4/groupProject/model/entity/InventoryItems.java
+++ src/main/java/edu/depaul/cdm/se352452group4/groupProject/model/entity/InventoryItems.java
@@ -1,4 +1,33 @@
 package edu.depaul.cdm.se352452group4.groupProject.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
 public class InventoryItems {
+
+
+
+    @Column(unique = true, name = "ItemsID")
+    private int itemID;
+
+    @Column(name = "Size")
+    private String size;
+
+    @Column(name = "Item Type")
+    private String itemType;
+
+    @Column(name = "ImageID")
+    private String imageID;
+
+    @Column(name = "InventoryCategory")
+    private String inventoryCategory;
+
+    @Column(name = "Quantity")
+    private int quantity;
+
+    @Column(name = "Price")
+    private float price;
 }
