5914993ae1f68789f7243389962e2eabeaa59582
Ross <ross.a.swanson@gmail.com>
2019-06-02 10:21:46 -0500

    Replaced some sections of the check out page with fragments

--- src/main/java/edu/depaul/cdm/se352452group4/groupProject/controller/StoreController.java
+++ src/main/java/edu/depaul/cdm/se352452group4/groupProject/controller/StoreController.java
@@ -3,6 +3,7 @@ package edu.depaul.cdm.se352452group4.groupProject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
@@ -54,8 +55,8 @@ public class StoreController implements WebMvcConfigurer {
 
 
         @GetMapping("/checkout")
-        public String checkoutRoute(){
-
+        public String checkoutRoute(Model model){
+            model.addAttribute("itemName", "Addidas Pullover");
             return  "checkout/index";
         }
 
