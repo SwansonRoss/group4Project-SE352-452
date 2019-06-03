b0d59db2ddb0fc228ffdd9e6947c2e7a24936f9e
Tuan Nguyen <tng.one@gmail.com>
2019-06-03 16:36:24 -0500

    minor changes

--- src/main/java/edu/depaul/cdm/se352452group4/groupProject/GroupProjectApplication.java
+++ src/main/java/edu/depaul/cdm/se352452group4/groupProject/GroupProjectApplication.java
@@ -12,7 +12,7 @@ public class GroupProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GroupProjectApplication.class, args);
                 System.out.println("Group 4 project running on port 8080");
-                
+
 	}
 
 //	@Bean
--- src/main/resources/db/migration/V1__Create_Schema.sql
+++ src/main/resources/db/migration/V1__Create_Schema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS GUCCl;
+CREATE SCHEMA IF NOT EXISTS GUCCl;
 
 CREATE TABLE IF NOT EXISTS account (
   accountID Integer  AUTO_INCREMENT NOT NULL,
@@ -6,8 +6,9 @@ CREATE TABLE IF NOT EXISTS account (
   lastName VARCHAR(50) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(20) NOT NULL,
-  subscribe BIT(0),
 
   PRIMARY KEY (accountID)
 
 );
+
+INSERT INTO ACCOUNT VALUES(1,'2010-04-29', 'tng.onw@gmail.com', 'tuan', 'Nguyen', 'chicken');
