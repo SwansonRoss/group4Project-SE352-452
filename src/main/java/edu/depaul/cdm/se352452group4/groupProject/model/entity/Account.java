87e10690fb757bf11d4b6d0981af07a8f20015c8
Heather Duke <hduke@mail.depaul.edu>
2019-06-03 02:30:47 -0500

    login Page Controller- getters & setters

--- src/main/java/edu/depaul/cdm/se352452group4/groupProject/model/entity/Account.java
+++ src/main/java/edu/depaul/cdm/se352452group4/groupProject/model/entity/Account.java
@@ -45,4 +45,57 @@ public class Account implements Serializable {
 //        this.email     = email;
 //        this.password  = password;
 //    }
+
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getAccountCreated() {
+        return accountCreated;
+    }
+
+    public void setAccountCreated(Date accountCreated) {
+        this.accountCreated = accountCreated;
+    }
 }
