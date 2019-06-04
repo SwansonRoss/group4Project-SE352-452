d89431d9094a0dbe6bf3d7ea3b86aae839ca9879
t1nguy3n <tng.one@gmail.com>
2019-06-04 16:24:49 -0500

    Created CustomerShipmentController removed vendorId for customerShipment

--- src/main/java/edu/depaul/cdm/se352452group4/groupProject/controller/AccountController.java
+++ src/main/java/edu/depaul/cdm/se352452group4/groupProject/controller/AccountController.java
@@ -4,11 +4,7 @@ package edu.depaul.cdm.se352452group4.groupProject.controller;
 import edu.depaul.cdm.se352452group4.groupProject.model.entity.Account;
 import edu.depaul.cdm.se352452group4.groupProject.model.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.EntityNotFoundException;
-import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -17,16 +13,20 @@ public class AccountController {
 
     private AccountRepository repo;
 
+    @Autowired
     public AccountController(AccountRepository repo) { this.repo = repo; }
 
     @GetMapping
     public Iterable<Account> getAllAccounts(){
-        System.out.println("all account ");
         return repo.findAll();
     }
 
     @PostMapping("user/createAccount")
     public Account createAccount(@RequestBody Account account){
+        while(getAccountById(account.getAccount_Id()).isPresent()) {
+            account.setAccount_Id(account.getAccount_Id()+1);
+        }
+
         return repo.save(account);
     }
 
@@ -39,5 +39,15 @@ public class AccountController {
     public Account getAccountByEmail (@PathVariable String email) {
         if(!email.endsWith(".com")) { email += ".com";}
         return repo.findByEmail(email);
+    }
+
+    @GetMapping("/user/{accountId}/delete")
+    public void deleteAccount(@PathVariable int accountId) {
+        Account account = repo.findById(accountId).orElseThrow(
+                ()-> new IllegalArgumentException("Account ID: " + accountId + " does not exist")
+        );
+
+       repo.delete(account);
+
     }
 }
