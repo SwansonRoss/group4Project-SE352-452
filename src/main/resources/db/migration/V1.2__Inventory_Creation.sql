6c1bdc1e87eaf141c00854ec3087ae0e0ca9a083
Jessica Bender <Jessicabender18@gmail.com>
2019-06-06 16:27:28 -0500

    old

---
+++ src/main/resources/db/migration/V1.2__Inventory_Creation.sql
@@ -0,0 +1,23 @@
+/* this is tables for the inventory categories */
+INSERT INTO Inventory_Category VALUES (1, 'men' );
+INSERT INTO Inventory_Category VALUES (2, 'woman' );
+INSERT INTO Inventory_Category VALUES (3, 'accessories' );
+INSERT INTO Inventory_Category VALUES (4, 'sales' );
+
+/*this is tables for all the invertory items */
+
+INSERT INTO Inventory_Type VALUES (1, 'shirt' );
+INSERT INTO Inventory_Type VALUES (2, 'suit' );
+INSERT INTO Inventory_Type VALUES (3, 'pants' );
+INSERT INTO Inventory_Type VALUES (4, 'dress' );
+
+/*hi*/
+
+INSERT INTO Inventory_Items VALUES (1, 'shirt' );
+
+
+
+
+
+
+
